aa9603ee95526a278f292a3050fd1d632b9663b5
Mariyaq28 <mariaqureshi560@gmail.com>
2025-05-03 13:25:11 +0500

    Update SimpCalculator.js

--- src/SimpCalculator.js
+++ src/SimpCalculator.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import './App.css';
 import { evaluate } from 'mathjs';
 
-
 function SimpCalculator() {   
   const [input, setInput] = useState('');
 
@@ -11,14 +10,13 @@ function SimpCalculator() {
     setInput(input + value);
   };
 
-
-const handleCalculate = () => {
-  try {
-    setInput(evaluate(input).toString());
-  } catch (error) {
-    setInput('Error');
-  }
-};
+  const handleCalculate = () => {
+    try {
+      setInput(evaluate(input).toString());
+    } catch (error) {
+      setInput('Error');
+    }
+  };
 
   const handleClear = () => {
     setInput('');
@@ -32,26 +30,25 @@ const handleCalculate = () => {
         <input type="text" value={input} readOnly />
       </div>
 
-    <div className="calculator-buttons">
-  <button onClick={handleClear}>C</button>
-  <button onClick={() => handleClick('1')}>1</button>
-  <button onClick={() => handleClick('2')}>2</button>
-  <button onClick={() => handleClick('3')}>3</button>
-  <button onClick={() => handleClick('+')}>+</button>
-  <button onClick={() => handleClick('4')}>4</button>
-  <button onClick={() => handleClick('5')}>5</button>
-  <button onClick={() => handleClick('6')}>6</button>
-  <button onClick={() => handleClick('-')}>-</button>
-  <button onClick={() => handleClick('7')}>7</button>
-  <button onClick={() => handleClick('8')}>8</button>
-  <button onClick={() => handleClick('9')}>9</button>
-  <button onClick={() => handleClick('*')}>*</button>
-  <button onClick={() => handleClick('0')}>0</button>
-  <button onClick={() => handleClick('.')}>.</button>
-  <button onClick={() => handleClick('/')}>/</button> 
-  <button onClick={handleCalculate}>=</button>
-</div>
-
+      <div className="calculator-buttons">
+        <button onClick={handleClear}>C</button>
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={() => handleClick('.')}>.</button>
+        <button onClick={() => handleClick('/')}>/</button>
+        <button onClick={handleCalculate}>=</button>
+      </div>
     </div>
   );
 }
